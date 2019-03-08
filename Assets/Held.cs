d24423afd6a34b39bac5a2631e26ffa5f355e548
Ben Greene <bgree052@gold.ac.uk>
2019-03-08 19:56:54 +0000

    3 objects test

---
+++ Assets/Held.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Held : MonoBehaviour {
+    //public Transform originalParent;
+
+	// Use this for initialization
+	void Start () {
+        //originalParent = gameObject.transform.parent;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(gameObject.transform.parent);
+        //SET SNAP OBJ & TARGET
+        //This won't work for held objects with multiple attach points.... :(
+        if (gameObject.transform.parent != null) {
+            if (gameObject.transform.parent.name == "RightHand")
+            {
+                GameObject.Find("RightHand").GetComponent<Snap>().snapObj = gameObject.transform;
+                Debug.Log("SNAP OBJ SET");
+            }
+            if (gameObject.transform.parent.name == "LeftHand")
+            {
+                //GameObject.Find("RightHand").GetComponent<Snap>().SnapTarget = gameObject.transform;
+                Debug.Log("SNAP TARGET SET");
+            }
+        }
+
+    }
+}
---
+++ Assets/Snap.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snap : MonoBehaviour, Collider_helper.collider_help_reciever
+{
+    public Transform snapObj;
+    //bool snapped = false;
+    private IEnumerator noSnap;
+    float lastSnapTime;
+    public bool cildsnap;
+    Snap target;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Snap Obj: "+snapObj);
+	}
+
+    //to determine whether or not you want to have the attached object be a child ('cild') set the boolean to true or false
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter");
+        target = other.GetComponent<Snap>();
+        if (target && !target.cildsnap && cildsnap)
+        {
+            if (lastSnapTime + 0.1f < Time.time)
+            {
+                snapObj.position = target.transform.position;
+                snapObj.rotation = target.transform.rotation;
+                snapObj.parent = target.transform;
+            }
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+
+        Debug.Log("exit");
+        if (other.transform.GetInstanceID() == target.transform.GetInstanceID())
+        {
+            lastSnapTime = Time.time;
+            snapObj.position = transform.position;
+            snapObj.rotation = transform.rotation;
+            snapObj.parent = transform;
+            //StartCoroutine(noSnap);
+        }
+    }
+}
