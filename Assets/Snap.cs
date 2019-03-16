8e444daf28ee313ada17bd498f46328124c6a3fa
Ben Greene <bgree052@gold.ac.uk>
2019-03-16 18:28:58 +0000

    New Snap Script
    Not working yet

--- Assets/Snap.cs
+++ Assets/Snap.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class Snap : MonoBehaviour, Collider_helper.collider_help_reciever
 {
-    public Transform snapObj;
-    //bool snapped = false;
-    private IEnumerator noSnap;
-    float lastSnapTime;
-    public bool cildsnap;
-    Snap target;
+    public Transform snapObj; //Object being snapped
+    float lastSnapTime; //store time when objects un-snapped
+    public bool childsnap; //to determine whether or not you want to have the attached object be a child set the boolean to true or false
+    Snap target; //is set to the snap script attached to the object you're snapping to
 
-	// Use this for initialization
+
 	void Start () {
         
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
         //Debug.Log("Snap Obj: "+snapObj);
 	}
 
-    //to determine whether or not you want to have the attached object be a child ('cild') set the boolean to true or false
+    
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("enter");
-        target = other.GetComponent<Snap>();
-        if (target && !target.cildsnap && cildsnap)
+        target = other.GetComponent<Snap>(); //target is set to other collider's object's snap script
+        if (target && !target.childsnap && childsnap) //if there is a target and that target's childsnap boolean is false and this object's boolean is true
         {
-            if (lastSnapTime + 0.1f < Time.time)
+            snapObj = target.transform;
+            if (lastSnapTime + 0.1f < Time.time) //if there has been enough time since the last decoupling
             {
+                //snap together
                 snapObj.position = target.transform.position;
                 snapObj.rotation = target.transform.rotation;
                 snapObj.parent = target.transform;
@@ -40,13 +40,12 @@ public class Snap : MonoBehaviour, Collider_helper.collider_help_reciever
     {
 
         Debug.Log("exit");
-        if (other.transform.GetInstanceID() == target.transform.GetInstanceID())
+        if (other.transform.GetInstanceID() == target.transform.GetInstanceID()) 
         {
             lastSnapTime = Time.time;
             snapObj.position = transform.position;
             snapObj.rotation = transform.rotation;
             snapObj.parent = transform;
-            //StartCoroutine(noSnap);
         }
     }
 }
